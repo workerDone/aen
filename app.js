aab4ca7dddf2c35732c2d3fe1905758172803e43
Mykola Chmut <chmutkola@gmail.com>
2019-07-20 11:53:12 +0300

    config

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+const config = require('config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,13 +15,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(
-  express.static(path.join(__dirname, 'public/public-view/dist/public-view'))
+  express.static(path.join(__dirname, config.get('HTML')))
 );
 
 app.get('*', (req, res, next) => {
   if (req.headers['upgrade-insecure-requests']) {
     res.sendFile(
-      path.resolve(__dirname, 'public/public-view/dist/public-view/index.html')
+      path.resolve(__dirname,  config.get('IndexHTML'))
     );
   } else {
     next();
